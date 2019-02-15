17d4f88e178fab226fbebc5669ce5e94eba37ff3
Ezequiel Arceo May <ezarceo85@gmail.com>
2019-02-15 10:22:26 -0600

    Arregla el indentado de los ejemplos

--- unidad1/clase-14-02-2019/areas.cpp
+++ unidad1/clase-14-02-2019/areas.cpp
@@ -4,67 +4,67 @@ PROPÓSITO: calcula el área de formas geométricas
 a partir de datos tomados de la consola
 USO: ejecute y proporcione los datos solicitados
 */
-#include <iostream>												// Lee desde y escribe hacia la consola
-#include <cmath>													// Libreria de funciones matemáticas
+#include <iostream>                       // Lee desde y escribe hacia la consola
+#include <cmath>                          // Libreria de funciones matemáticas
 
-using namespace std;											// Se usa el espacio de nombres estándar
+using namespace std;                      // Se usa el espacio de nombres estándar
 
 float areaCirculo(float radio) {
-	float area = 0;													// Defino una variable para guardfar el �rea
-	float PI = 3.1415;											// Defino de la constante PI
-	area = PI * radio * radio;							// Calculo el área
-	return area;														// Devuelve el valor área calculada
+  float area = 0;                         // Defino una variable para guardfar el área
+  float PI = 3.1415;                      // Defino de la constante PI
+  area = PI * radio * radio;              // Calculo el área
+  return area;                            // Devuelve el valor área calculada
 }
 
 float areaRectangulo(float base, float altura) {
-	float area = 0;													// Defino una variable para guardfar el �rea
-	area = base * altura;										// Calculo el área
-	return area;														// Devuelve el valor área calculada
+  float area = 0;                         // Defino una variable para guardfar el área
+  area = base * altura;                   // Calculo el área
+  return area;                            // Devuelve el valor área calculada
 }
 
 float areaTriangulo(float base, float altura) {
-	float area = 0;													// Defino una variable para guardfar el �rea
-	area = base * altura / 2;								// Calculo el área
-	return area;														// Devuelve el valor área calculada
+  float area = 0;                          // Defino una variable para guardfar el área
+  area = base * altura / 2;                // Calculo el área
+  return area;                             // Devuelve el valor área calculada
 }
 
 
 int main() {
-	float area = 0;
-	float radio = 0;															// Variable flotante para contener un valor de radio
-	float base = 0;																// Variable flotante para contener un valor de base
-	float altura = 0;															// Variable flotante para contener un valor de altura
+  float area = 0;
+  float radio = 0;                          // Variable flotante para contener un valor de radio
+  float base = 0;                           // Variable flotante para contener un valor de base
+  float altura = 0;                         // Variable flotante para contener un valor de altura
 
-	cout << "[INFO] Trabajamos con el área un círculo" << endl;
-	cout << "Ingrese ell valor del radio: ";			// Pedimos un valor del radio al usuario, 
-	cin >> radio;														 			// guardamos el valor del radio en la variable "radio"
-	area = areaCirculo(radio);										// llamamos a la función que calcula el área
-	cout << "El area del circulo de radio " 
-	<< radio << " es " << area << endl;
-	system("pause");
+  cout << "[INFO] Trabajamos con el área un círculo" << endl;
+  cout << "Ingrese ell valor del radio: ";  // Pedimos un valor del radio al usuario, 
+  cin >> radio;                             // guardamos el valor del radio en la variable "radio"
+  area = areaCirculo(radio);                // llamamos a la función que calcula el área
+  cout << "El area del circulo de radio " 
+  << radio << " es " << area << endl;
+  system("pause");
 
 
-	cout << "[INFO] Trabajamos con el área un rectángulo" << endl;
-	cout << "Ingrese del valor de la base: ";			// Pedimos un valor de una base al usuario, 
-	cin >> base;																	// guardamos el valor de base la variable "base"
-	cout << "Ingrese del valor de la altura: ";		// Pedimos un valor de una base al usuario, 
-	cin >> altura;																// guardamos el valor de base la variable "base"
-	area = areaRectangulo(base, altura);					// llamamos a la función que calcula el área
-	cout << "El area del rectangulo de base " 
-	<< base << " y altura " << altura << " es " << area << endl;
-	system("pause");
-	
+  cout << "[INFO] Trabajamos con el área un rectángulo" << endl;
+  cout << "Ingrese del valor de la base: ";   // Pedimos un valor de una base al usuario, 
+  cin >> base;                                // guardamos el valor de base la variable "base"
+  cout << "Ingrese del valor de la altura: "; // Pedimos un valor de una base al usuario, 
+  cin >> altura;                              // guardamos el valor de base la variable "base"
+  area = areaRectangulo(base, altura);        // llamamos a la función que calcula el área
+  cout << "El area del rectangulo de base " 
+  << base << " y altura " << altura << " es " << area << endl;
+  system("pause");
+  
 
-	cout << "[INFO] Trabajamos con el área un triángulo" << endl;
-	cout << "Ingrese del valor de la base: ";			// Pedimos un valor de una base al usuario, 
-	cin >> base;																	// guardamos el valor de base la variable "base"
-	cout << "Ingrese del valor de la altura: ";		// Pedimos un valor de una base al usuario, 
-	cin >> altura;																// guardamos el valor de base la variable "base"
-	area = areaTriangulo(base, altura);					// llamamos a la función que calcula el área
-	cout << "El area del triángulo de base " 
-	<< base << " y altura " << altura << " es " << area << endl;
-	system("pause");
+  cout << "[INFO] Trabajamos con el área un triángulo" << endl;
+  cout << "Ingrese del valor de la base: ";     // Pedimos un valor de una base al usuario, 
+  cin >> base;                                  // guardamos el valor de base la variable "base"
+  cout << "Ingrese del valor de la altura: ";   // Pedimos un valor de una base al usuario, 
+  cin >> altura;                                // guardamos el valor de base la variable "base"
+  area = areaTriangulo(base, altura);           // llamamos a la función que calcula el área
+  cout << "El area del triángulo de base " 
+  << base << " y altura " << altura << " es " << area << endl;
+  system("pause");
 
-	return 0;
+  return 0;
 }
 
--- unidad1/clase-14-02-2019/vectores.cpp
+++ unidad1/clase-14-02-2019/vectores.cpp
@@ -5,46 +5,46 @@ de la consola, los imprime, y calcula el promedio
 USO: ejecuta e inserta (escribe) números cada vez que se
 le solicite. Escribe un número negativo para salir.
 */
-#include <iostream>										// Lee desde y escribe hacia la consola
-#include <vector>											// Permite hacer colecciones de datos de un mismo tipo
+#include <iostream>                   // Lee desde y escribe hacia la consola
+#include <vector>                     // Permite hacer colecciones de datos de un mismo tipo
 
-using namespace std;									// Usa el espacio de nombres est�ndar
+using namespace std;                  // Usa el espacio de nombres est�ndar
 
 int main() {
-	/* Definición de variables */
-	vector<float> calificaciones;				// Define un vector vacio de enteros
-	float calificacion = 0;							// Guarda el último número introducido
-	float suma = 0;											// Variable para guardar la suma de enteros en el vector
-	float promedio;											// Declaro una variable para el promedio
-	bool pide_otro = true;							// Variable para determinar si pido otro número al usuario
+  /* Definición de variables */
+  vector<float> calificaciones;       // Define un vector vacio de enteros
+  float calificacion = 0;             // Guarda el último número introducido
+  float suma = 0;                     // Variable para guardar la suma de enteros en el vector
+  float promedio;                     // Declaro una variable para el promedio
+  bool pide_otro = true;              // Variable para determinar si pido otro número al usuario
 
-	/* Pide números al usuario hasta que inserta -1.
-	Si el número insertado no es -1, se guarda en el vector
-	*/
-	cout << "[INFO] Ingrese un número negativo para salir" << endl;
-	while ( pide_otro ) {															// Para iterar una cantidad indefinido de veces se usa while
-		cout << "Ingrese una calificacion: ";						// Dá instrucciones al usuario
-		cin >> calificacion;														// Leo información de la consola
-		if ( calificacion > 0.0 ) {
-			// Esto se ejecuta si calificacion > 0.0 
-			pide_otro = true;
-			calificaciones.push_back(calificacion);				// Inserta el número al final del vector
-		}else{
-		// Esto se ejecuta si calificacion < 0.0 
-			pide_otro = false;
-		}
-	}
+  /* Pide números al usuario hasta que inserta -1.
+  Si el número insertado no es -1, se guarda en el vector
+  */
+  cout << "[INFO] Ingrese un número negativo para salir" << endl;
+  while ( pide_otro ) {                       // Para iterar una cantidad indefinido de veces se usa while
+    cout << "Ingrese una calificacion: ";     // Dá instrucciones al usuario
+    cin >> calificacion;                      // Leo información de la consola
+    if ( calificacion > 0.0 ) {
+      // Esto se ejecuta si calificacion > 0.0 
+      pide_otro = true;
+      calificaciones.push_back(calificacion);  // Inserta el número al final del vector
+    }else{
+    // Esto se ejecuta si calificacion < 0.0 
+      pide_otro = false;
+    }
+  }
 
-	/* Imprime los elementos del vector */
-	for (int i = 0; i < calificaciones.size(); i++) {	// 
-		cout << "La calificacion en la posicion " << i << " es " << calificaciones[i] << endl;
-		suma = suma + calificaciones[i];								// Añade a "suma" el valor de "calificaciones" en la posición i
-	}
+  /* Imprime los elementos del vector */
+  for (int i = 0; i < calificaciones.size(); i++) { // Usamos ciclos for para iterar un número conocido de veces
+    cout << "La calificacion en la posicion " << i << " es " << calificaciones[i] << endl;
+    suma = suma + calificaciones[i];          // Añade a "suma" el valor de "calificaciones" en la posición i
+  }
 
-	/* Calcula el promedio y lo imprime en pantalla */
-	if ( calificaciones.size() != 0 ) {								// Compruebo que el vector no está vacio, para evitar división entre cero
-		promedio = suma / calificaciones.size();
-		cout << "El promedio es: " << promedio << endl;
-	}
-	system("pause");
+  /* Calcula el promedio y lo imprime en pantalla */
+  if ( calificaciones.size() != 0 ) {         // Compruebo que el vector no está vacio, para evitar división entre cero
+    promedio = suma / calificaciones.size();
+    cout << "El promedio es: " << promedio << endl;
+  }
+  system("pause");
 }
